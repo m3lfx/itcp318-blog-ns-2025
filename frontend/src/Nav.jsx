e7a0500d70cc25182e9c8f30c1b90d295a4d26dd
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-19 14:23:19 +0800

    first commit

---
+++ frontend/src/Nav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul className="nav nav-tabs">
+                <li className="nav-item pr-3 pt-3 pb-3">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="nav-item pr-3 pt-3 pb-3">
+                    <Link to="/create">Create</Link>
+                </li>
+                {/* <li
+                    // onClick={() => logout(() => navigate('/'))}
+                    className="nav-item ml-auto pr-3 pt-3 pb-3"
+                    style={{ cursor: 'pointer' }}
+                >
+                    Logout
+                </li>  */}
+                <li className="nav-item ml-auto pr-3 pt-3 pb-3">
+                    <Link to="/login">Login</Link>
+                </li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
