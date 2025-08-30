8b1aa2a0bc5fc16887ad522b58ed10e8dbf8706e
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-30 13:52:03 +0800

    post create

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const postRoutes = require('./routes/post');
+
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(cors());
+
+app.use('/api/v1', postRoutes);
+
+
 module.exports = app
--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,2 +1,23 @@
 const Post = require('../models/post');
 const slugify = require('slugify');
+
+exports.create = async (req, res) => {
+    console.log(req.body);
+    const { title, content, user } = req.body;
+    const slug = slugify(title);
+    // // validate
+    switch (true) {
+        case !title:
+            return res.status(400).json({ error: 'Title is required' });
+
+        case !content:
+            return res.status(400).json({ error: 'Content is required' });
+
+    }
+    // // create post
+    const post = await Post.create({ title, content, user, slug })
+    // console.log(post)
+    if (post)
+        return res.status(200).json(post)
+    return res.status(400).json({ error: "post create error" })
+};
