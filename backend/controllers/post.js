93a5ecb37e89b0cfdc7777b96a463d3562e89669
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-30 14:06:48 +0800

    post list4

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -21,3 +21,14 @@ exports.create = async (req, res) => {
         return res.status(200).json(post)
     return res.status(400).json({ error: "post create error" })
 };
+
+exports.list = async (req, res) => {
+    const posts = await Post.find({})
+        .limit(10)
+        .sort({ createdAt: 'desc' })
+        .exec();
+
+    if (posts)
+        return res.status(200).json(posts)
+    return res.status(400).json({ error: "list posts error" })
+};
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, } = require('../controllers/post');
+const { create, list } = require('../controllers/post');
 
 
 router.post('/post', create);
+router.get('/posts', list);
 
 module.exports = router;
