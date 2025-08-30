d13f7a967c934033fb3979a1ebc67b076b7a24a3
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-30 15:23:05 +0800

    create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav'
 
 import Create from './Create';
+
 const App = () => {
+
+  const [count, setCount] = useState(0); 
   return (
     <div className="App">
-      home
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+
+
     </div>
-)
+  )
 }
 
 export default App
--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,28 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Nav from './Nav';
 import Title from './Title';
+import axios from 'axios'
 
 
 const Create = () => {
+    const navigate = useNavigate()
+    const [state, setState] = useState({
+        title: '',
+        content: '',
+        user: '',
+    });
+
+    const handleChange = name => event => {
+        console.log('name', name, 'event', event.target.value);
+        
+        setState({ ...state, [name]: event.target.value });
+    };
+    console.log("state", state)
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        // console.table({ title, content, user });
+        axios
+            .post(`${import.meta.env.VITE_APP_API}post`, { title, content, user }
+            ).then(response => {
+                console.log(response);
+                
+                setState({ ...state, title: '', content: '', user: '' });
+                // show sucess alert
+                navigate('/')
+            })
+            .catch(error => {
+                console.log(error.response);
+                alert(error.response.data.error);
+            });
+    };
+
+    const { title, content, user } = state;
+    console.log(title,user,content)
+    
     return (
         <div className="container p-5">
            
-            <Title name="create blog post"  subTitle="my sub title"/>
-            <h1>CREATE POST</h1>
+            <Title name="create blog post" />
+           
             <br />
-            <form >
+            <form onSubmit={handleSubmit} >
                 <div className="form-group">
                     <label className="text-muted">Title</label>
-                    <input type="text" className="form-control" placeholder="Post title" required />
+                    <input type="text" className="form-control" placeholder="Post title" required onChange={handleChange('title')} value={title}  />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">Content</label>
-                    <textarea type="text" className="form-control" placeholder="Write something.." required  />
+                    <textarea type="text" className="form-control" placeholder="Write something.." required onChange={handleChange('content')} value={content}  />
                 </div>
                 <div className="form-group">
                     <label className="text-muted">User</label>
-                    <input type="text" className="form-control" placeholder="Your name" required />
+                    <input type="text" className="form-control" placeholder="Your name" required onChange={handleChange('user')} value={user}  />
                 </div>
                 <div>
                     <button className="btn btn-primary">Create</button>
