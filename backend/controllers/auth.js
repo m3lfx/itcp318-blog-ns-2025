9df7d15c1d13e379860769d0406a3ba71d851aea
rommel dalisay <rommelmdalisay@gmail.com>
2025-09-02 15:03:35 +0800

    jwt

---
+++ backend/controllers/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+var { expressjwt: jsonToken } = require("express-jwt");
+
+
+exports.login = (req, res) => {
+    const { name, password } = req.body;
+    if (password === process.env.PASSWORD) {
+        // generate token and send to client/react
+        const token = jwt.sign({ name }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        return res.json({ token, name });
+    } else {
+        return res.status(400).json({
+            error: 'Incorrect password!'
+        });
+    }
+};
+
+exports.requireSignin = jsonToken({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"]
+});
