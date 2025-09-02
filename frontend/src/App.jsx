16382a1a55c7c7d0ad6cf5b19b4de86ae52a372b
rommel dalisay <rommelmdalisay@gmail.com>
2025-09-02 13:45:27 +0800

    index

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,53 @@
-import React, { useState } from 'react'
-import Nav from './Nav'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
-import Create from './Create';
-
+import axios from 'axios';
 const App = () => {
 
-  const [count, setCount] = useState(0); 
+  const [posts, setPosts] = useState([])
+
+ const fetchPosts = () => {
+    axios
+      .get(`${import.meta.env.VITE_APP_API}posts`)
+      .then(response => {
+        console.log(response);
+        setPosts(response.data);
+      })
+      .catch(error => alert('Error fetching posts'));
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, [])
+  console.log(posts)
+
   return (
     <div className="App">
-      <div>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-      </div>
+       {posts.length > 0 ? posts.map((post) => (
+        <div className="row" key={post._id} style={{ borderBottom: '1px solid silver' }}>
+          <div className="col pt-3 pb-2">
+            <Link to={`/post/${post.slug}`}>
+              <h2>{post.title}</h2>
+            </Link>
+            <p className="lead">{post.content.substring(0, 100)}</p>
+            <p>
+              Author <span className="badge" style={{ color: 'red' }}>{post.user}</span> Published on{' '}
+              <span className="badge" style={{ color: 'red' }}>{new Date(post.createdAt).toLocaleString()}</span>
+            </p>
+            <Link to={`/post/update/${post.slug}`} className="btn btn-sm btn-outline-warning">
+              Update
+            </Link>
+             <button
+                  // onClick={() => deleteConfirm(post.slug)}
+                  className="btn btn-sm btn-outline-danger ml-1"
+                >
+                  Delete
+                </button>
+
+          </div>
+        </div>
+      )) : <h1>no posts</h1>}
+     
 
 
     </div>
